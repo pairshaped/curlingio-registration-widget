55ec814715db1b473a917c31f9e273f9ce1172b7
Dave Rapin <dave@rapin.com>
2017-09-01 16:20:31 -0400

    Changed url back to prod env

--- src/Rest.elm
+++ src/Rest.elm
@@ -16,8 +16,8 @@ fetchProducts accessKey =
 
 productsUrl : AccessKey -> String
 productsUrl accessKey =
-    -- "https://curling.io/api/organizations/" ++ accessKey ++ "/products"
-    "http://localhost:4000/products"
+    -- "http://localhost:4000/products"
+    "https://curling.io/api/organizations/" ++ accessKey ++ "/products"
 
 
 productsDecoder : Decode.Decoder (List Product)
